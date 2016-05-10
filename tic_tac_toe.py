b5bc0682cf8171f51f296e141543acfc96a60ca2
Hoang Pham <bobobis@Ans-iMac-1627.local>
2016-05-09 21:30:10 -0700

    Playable Version

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -11,13 +11,8 @@ class tic_tac_toe():
 		self.rows = []
 		self.diagonals = []
 		self.moves = [[],[]]
-
-
-	# def create_grid(self):
-	# 	GRID_SIZE = 3
-
-	# 	for a in range(GRID_SIZE):
-	# 		self.grid.append([[], [], []])
+		self.gameover = False
+		self.players = 2
 
 	def display_grid(self):
 		for x in range(len(self.grid)):
@@ -30,10 +25,17 @@ class tic_tac_toe():
 	def place_x_at(self, x):
 		self.grid[x-1] = ["X"]
 		self.moves[0].append(x-1)
+		self.check_for_win()
 
 	def place_o_at(self, o):
 		self.grid[o-1] = ["O"]
 		self.moves[1].append(o-1)
+		self.check_for_win()
+
+	def mark(self, pos, symbol):
+		self.grid[pos-1] = [symbol]
+		self.moves[0].append(pos-1)
+		self.check_for_win()
 
 	def get_columns(self):
 		temp = ""
@@ -41,8 +43,6 @@ class tic_tac_toe():
 			temp += self.grid[x][0] + self.grid[x + self.GRID_SIZE][0] + self.grid[x + 2*self.GRID_SIZE][0]
 			self.columns.append(temp)
 			temp = ""
-
-		print(self.columns)
 
 	def get_rows(self):
 		temp = ""
@@ -53,53 +53,50 @@ class tic_tac_toe():
 				self.rows.append(temp)
 				temp = ""
 
-		print(self.rows)
 
 	def get_diagonals(self):
 		temp = ""
 		temp2 = ""
 		counter1 = 0
 		counter2 = 0
+
 		for x in range(self.GRID_SIZE):
 			if x == 0:
 				temp += self.grid[x][0]
-				temp2 += self.grid[self.GRID_SIZE][0]
+				temp2 += self.grid[self.GRID_SIZE - 1][0]
 				counter1, counter2 = 0, 2
 			else:
-				counter1, counter2 = counter1 + 3, counter2 + 2
+				counter1, counter2 = counter1 + (self.GRID_SIZE + 1), counter2 + (self.GRID_SIZE - 1)
 				temp += self.grid[counter1][0]
 				temp2 += self.grid[counter2][0]
 		self.diagonals.append(temp)
 		self.diagonals.append(temp2)
 
-		print(self.diagonals)
-
-
-
-
-		pass
-
 	def check_for_win(self):
-		pass
+		self.get_columns()
+		self.get_rows()
+		self.get_diagonals()
+		x = 'XXX'
+		o = 'OOO'
 
+		if x in self.diagonals or x in self.rows or x in self.columns:
+			self.display_grid()
+			print("X WON!")
+			self.gameover = True
+		elif o in self.diagonals or o in self.rows or o in self.columns:
+			self.display_grid()
+			print("O WON!")
+			self.gameover = True
+
+	def run(self):
+		sym = 'O'
+
+		while not self.gameover:
+			sym = 'X' if sym == 'O' else 'O'
+			self.display_grid()
+			a = int(input("{}'s Turn".format(sym)))
+			self.mark(a, sym)
 
 if __name__ == "__main__":
 	a = tic_tac_toe()
-	print(a.grid)
-	print("")
-	a.display_grid()
-	a.place_x_at(4)
-	a.place_x_at(1)
-	a.place_x_at(7)
-	print("")
-	a.display_grid()
-	a.place_o_at(5)
-	print("")
-	a.display_grid()
-	print("")
-	print(a.grid)
-	print("")
-	a.get_columns()
-	print("")
-	a.get_diagonals()
-	input()
+	a.run()
